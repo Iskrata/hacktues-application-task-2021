d8639025c369d7770b66dd827b1eb25ce094a89d
Iskren Alexandrov <iskren.alexandrov@gmail.com>
2021-07-04 00:31:52 +0300

    Fixes & final changes

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const route = express.Router()
+
+
 require('dotenv/config');
 
 const connectDB = require('./server/database/connection');
 
 const app = express();
 const port = 3000
+
+var cors = require('cors');
+app.use(cors({
+    origin: ["http://localhost:8200", "http://127.0.0.1:8200"],
+    credentials: true,
+}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
